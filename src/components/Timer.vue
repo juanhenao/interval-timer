6a37922985f33e1cf1a0449948a8fbdf6731b531
Juan Henao <juanhenao10@gmail.com>
2020-02-11 06:54:19 +0100

    Begin with progress circle

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="timer">
-    <p>{{currentTime}}</p>
+    <div>
+      <p>{{currentTime}}</p>
+    </div>
+    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="50" cy="50" r="50" stroke-dasharray="565.48"/>
+    </svg>
   </div>
 </template>
 
