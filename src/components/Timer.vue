7b33f38f54fb15613f47b31188aa031357771ccc
Juan Henao <juanhenao10@gmail.com>
2020-02-23 14:51:28 +0100

    Show progress with circle

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="timer">
-    <div>
+    <div class="text-timer">
       <p>{{currentTime}}</p>
     </div>
-    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="50" cy="50" r="50" stroke-dasharray="565.48"/>
+    <svg viewBox="0 0 106 106" xmlns="http://www.w3.org/2000/svg">    
+      <circle cx="53" cy="53" :r="radius"/>
+      <circle
+        cx="53"
+        cy="53"
+        :r="radius"
+        :stroke-dasharray="perimeter"
+        :stroke-dashoffset="currentPct"
+        class="progress-bar"
+      />
     </svg>
   </div>
 </template>
@@ -22,10 +30,15 @@ export default Vue.extend({
       time: 0,
       currentInterval: 0,
       intervals: [10 * 1000, 15 * 1000],
-      currentPhase: 0
+      currentPhase: 0,
+      currentPct: 0,
+      radius: 50
     };
   },
   computed: {
+    perimeter(): number {
+      return 2 * Math.PI * this.radius;
+    },
     currentTime(): string {
       return new Date(this.time).toISOString().slice(14, 19);
     }
@@ -33,14 +46,19 @@ export default Vue.extend({
   methods: {
     startTimer() {
       this.currentInterval = setInterval(() => {
-        this.time = this.time + 1000;
+        this.time = this.time + 10;
+        this.setStrokeDashOffset(this.time/this.intervals[this.currentPhase]*100);
         if (this.time > this.intervals[this.currentPhase]) {
           clearInterval(this.currentInterval);
           this.currentPhase = (this.currentPhase + 1) % this.intervals.length;
           this.time = 0;
+          this.currentPct=0;
           this.startTimer();
         }
-      }, 1000);
+      }, 10);
+    },
+    setStrokeDashOffset(value: number){      
+      this.currentPct = ((100-value)/100)*this.perimeter;
     }
   },
   mounted: function() {
