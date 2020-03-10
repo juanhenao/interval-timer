5efca6ccd6e46d5d75528310d503f8707bc5b3fb
Juan Henao <juanhenao10@gmail.com>
2020-03-10 06:13:49 +0100

    Add vuex and move bulma import

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex';
 import App from './App.vue'
 import './registerServiceWorker'
 import './styles/main.scss'
-import './../node_modules/bulma/css/bulma.css';
+
 
 import router from './routes';
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(Vuex);
 
 new Vue({
   render: h => h(App),
